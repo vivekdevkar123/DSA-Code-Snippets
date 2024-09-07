e0ee63db7772c893a3c6fce20149317d62a9ba9c
Vivek devkar <mrvivekdevkar123@gmail.com>
2024-09-08 01:21:34 +0530

    Update Cycle_in_Graph.py

--- Graph/Cycle_in_Graph.py
+++ Graph/Cycle_in_Graph.py
@@ -1,1 +1,10 @@
-
+bool isCycle(unordered_map<int,vector<int>>&m,int curr,int parant,vector<int>&vis){
+    if(vis[curr] && parant != curr) return true;
+    vis[curr] = 1;
+    for(int u:m[curr]){
+        if(parant == u) continue;
+        if(vis[u]) return true;
+        if(solve(m,u,curr,vis)) return true;
+    }
+    return false;
+}
