4d154451114d4d8c1cbaa8c40cb140719b0ea71b
Vivek devkar <mrvivekdevkar123@gmail.com>
2024-10-08 15:22:30 +0530

    Update and rename Floyd.py to Floyd Warshall Algorithm.py

--- Graph/Floyd Warshall Algorithm.py
+++ Graph/Floyd Warshall Algorithm.py
@@ -1,13 +1,11 @@
+# To get the minimum distance of each node from every node
+
 void Floyd_Warshall_Algorithm(vector<vector<int>>& grid){
     int n = grid.size();
-    
     for(int via = 0; via < n; via++) {
-        
         for(int i = 0; i<n; i++) {
             for(int j = 0; j<n; j++) {
-                
                 grid[i][j] = min(grid[i][j], grid[i][via] + grid[via][j]);
-                
             }
         }
     }
